ef7424763c36d8d0b7a3118ae1f50c72c8f32321
Ebube111 <ebubeagwaze@gmail.com>
2022-01-30 12:28:40 +0100

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -7,41 +7,41 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-        return "Hello HBNB!"
+    return "Hello HBNB!"
 
 
-    @app.route('/hbnb')
-    def hbnb():
-            return "HBNB"
+@app.route('/hbnb')
+def hbnb():
+    return "HBNB"
 
 
-        @app.route('/c/<string:s>')
-        def c(s):
-                new_s = s.replace("_", " ")
-                    return "C {}".format(new_s)
+@app.route('/c/<string:s>')
+def c(s):
+    new_s = s.replace("_", " ")
+    return "C {}".format(new_s)
 
 
-                @app.route('/python', strict_slashes=False)
-                @app.route('/python/<string:s>')
-                def python(s="is cool"):
-                        new_s = s.replace("_", " ")
-                            return "Python {}".format(new_s)
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:s>')
+def python(s="is cool"):
+    new_s = s.replace("_", " ")
+    return "Python {}".format(new_s)
 
 
-                        @app.route('/number/<int:n>')
-                        def number(n):
-                                return "{} is a number".format(n)
+@app.route('/number/<int:n>')
+def number(n):
+    return "{} is a number".format(n)
 
 
-                            @app.route('/number_template/<int:n>')
-                            def number_template(n):
-                                    return render_template('5-number.html', num=n)
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', num=n)
 
 
-                                @app.route('/number_odd_or_even/<int:n>')
-                                def number_odd_or_even(n):
-                                        return render_template('6-number_odd_or_even.html', num=n)
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', num=n)
 
 
-                                    if __name__ == "__main__":
-                                            app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
